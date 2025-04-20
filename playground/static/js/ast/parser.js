a7b6ec3d234c4daa7f55efeac6fc63394926b9b1
CiroZDP <cirod307@gmail.com>
2025-04-20 20:39:32 +0200

    ➕ Add `ASTFunctionParser`

--- playground/static/js/ast/parser.js
+++ playground/static/js/ast/parser.js
@@ -1,7 +1,11 @@
 export class ASTParser {
-    constructor(tokenStream) {
-        this.stream = tokenStream;
+
+    constructor(stream) {
+        this.stream = stream;
         this.tree   = [];
+
+        this.funcParser = new ASTFunctionParser(this.tree, stream);
+        //this.varParser  = new ASTVariableParser(this.tree, stream);
     }
 
     parse() {
@@ -10,7 +14,7 @@ export class ASTParser {
 
             // Match a function
             if (token === 'fn') {
-                const fn = new window.ASTFunctionTemplate().fill(this.stream);
+                this.funcParser.parse();
             }
 
             // Match a variable
@@ -24,10 +28,62 @@ export class ASTParser {
                 console.warn("Unknown construct:", token);
                 this.stream.skip();
             }
-        return this.tree;
+
         }
 
+        return this.tree;
     }
+}
+
+export class ASTFunctionParser {
+
+    constructor(tree, stream) {
+        this.stream = stream;
+        this.tree   = tree;
+    }
+
+    parse() {
+        const fn = new ASTFunctionTemplate().fill(this.stream);
+        let block = {
+            type: "FunctionDeclaration",
+            returnType: "void",
+            body: [],
+            ...fn
+        };
+
+        if (this.stream.peek() !== "{") {
+            if (!this.stream.peekTypeEquals(["ID", "TYPE"])) {
+                // TODO: error handling
+                return console.error("Expected a valid return type"), block;
+            }
+
+            block.returnType = this.stream.pop();
+        }
+
+        block.body = this.parseBody();
+        this.tree.push(block);
+    }
+
+    parseBody() {
+        let body = [];
+        if (this.stream.peek() !== "{") {
+            // TODO: error handling
+            return console.error("Expected a valid return type"), block;
+        }
+
+        console.log(this.stream.remaining());
+        while (this.stream.peek() !== "}"
+            && this.stream.remaining() >= 1)
+        {
+            console.log("hola");
+            const parser = new ASTParser(this.stream);
+            body = parser.parse();
+            console.log(body);
+        }
+
+        return body;
+    }
+
 }
 
 window.ASTParser = ASTParser;
