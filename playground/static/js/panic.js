a7c35569df5c46f13ae556c2b7fcea46bb5d3f5d
CiroZDP <cirod307@gmail.com>
2025-04-21 13:06:54 +0200

    ➕ `panic.js`: Add panic function

---
+++ playground/static/js/panic.js
@@ -0,0 +1,135 @@
+const sliceError = errno => [ getPhase(errno), getModule(errno), getRawError(errno) ];
+const codeToHex  = errno => errno.toString(16).padStart(6, "0");
+
+const phases = {
+    0x1: "Lexer",
+    0x2: "AST",
+    0x3: "Parser",
+    0x4: "Semantic",
+    0x5: "Interpreter"
+};
+
+export function panic(error, line, col, sourceLine) {
+    const { code, message } = error;
+
+    const filepath = './src/main.rt';
+    const phase    = phases[code >> 20] || "Unknown";
+
+    const suggestion = "Did you mean Gonorrea?";
+    const customMessage = null;
+
+    /* Display */
+    let detailedMessage = "";
+    {
+        detailedMessage += `\n Error[0x${codeToHex(code)}]: ${message}`;
+        if (customMessage)
+            detailedMessage += `: ${customMessage}\n`;
+
+        detailedMessage += `\n     ┌─ ${filepath}:${line}:${col}`;
+        detailedMessage += `\n     │`;
+
+        // This will prevent the digits from moving everything to the right
+        detailedMessage += `\n ${line.toString().padStart(3, ' ')} │ ${sourceLine}`;
+
+        // Wait until the wrong part and repeat '^' until the end of the line
+        detailedMessage += `\n     │${' '.repeat(col)}${'^'.repeat(sourceLine.length - col + 1)} `;
+        if (suggestion)
+            detailedMessage += suggestion; // ^^^^^ Did you mean...?
+
+    }
+    displayErrorPopup(detailedMessage);
+}
+
+function displayErrorPopup(detailedMessage) {
+    const params = {
+        scrollbars: false,
+        resizable: true,
+        status: false,
+        location: false,
+        toolbar: false,
+        width: 560,
+        height: 400,
+        left: -1000,
+        top: -1000
+    }
+
+    // { scrollbars: false } -> "scrollbars=false"
+    const fmtParams = Object.entries(params).map(a => `${a[0]}=${a[1]}`).join()
+    let error_popup = window.open('about:blank', "test", fmtParams);
+
+    error_popup.document.head.innerHTML =`
+        <title>Ruthenium Interpreter -> Error</title>
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+
+            }
+
+            body {
+                padding: 12px;
+                background-color: #222;
+            }
+
+            textarea {
+                outline: none;
+                width: 100%;
+                height: 100%;
+                font-family 'Space Mono', sans-serif;
+                font-size: 12pt;
+                background: #222;
+                border: none;
+                color: #fff;
+            }
+        </style>
+    `;
+
+    error_popup.document.body.innerHTML = `<textarea readonly="true">${detailedMessage}</textarea>`;
+}
+
+export const Errors = {
+    LEXER: {
+        Invalid_Char: {
+            code: 0x100001,
+            message: "Invalid character"
+        },
+        Unclosed_String: {
+            code: 0x100002,
+            message: "Unclosed string literal"
+        }
+    },
+    AST: {
+        Malformed_Variable: {
+            code: 0x200001,
+            message: "Malformed variable name"
+        },
+        Duplicate_Declaration: {
+            code: 0x200002,
+            message: "Duplicate variable declaration"
+        },
+        Missing_Identifier: {
+            code: 0x200003,
+            message: "Expected identifier"
+        }
+    },
+    PARSER: {
+        Unexpected_Token: {
+            code: 0x300001,
+            message: "Unexpected token"
+        },
+        Unclosed_Parentheses: {
+            code: 0x300002,
+            message: "Unclosed parentheses"
+        }
+    },
+};
+
+
+function getMessage(phase, module, error) {
+    return errors[phase][error]
+}
+
+window.panic = panic;
+window.Errors = Errors;
