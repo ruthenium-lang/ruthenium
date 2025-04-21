bbfc4d494f1fc0ee14236b9f33e8a93fea049ee0
CiroZDP <cirod307@gmail.com>
2025-04-21 09:48:58 +0200

    🛠️ fix: Make `ASTOperatorPattern` public

--- playground/static/js/ast/patterns/operator.pattern.js
+++ playground/static/js/ast/patterns/operator.pattern.js
@@ -1,4 +1,5 @@
 export class ASTOperatorPattern {
+
     constructor(stream) {
         this.stream = stream;
     }
@@ -7,16 +8,19 @@ export class ASTOperatorPattern {
         let obj = {
             type: "Operator"
         }
-        
+
         // Check if its an operator (+, -, *, /)
         const operators = ["+", "-", "*", "/"];
         if (!operators.includes(this.stream.peek()))
             return null;
-        
+
         obj.operator = this.stream.pop();
         obj.left = this.stream.peek(-1);
         obj.right = this.stream.pop();
-        
+
         return obj;
     }
+
 }
+
+window.ASTOperatorPattern = ASTOperatorPattern;
