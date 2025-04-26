20212db3763d1db87a1359f34d8a692ec4eadd7c
CiroZDP <cirod307@gmail.com>
2025-04-26 21:35:26 +0200

    ➕ `character.js`: Add function `toChar`

--- playground/static/js/compiler/util/character.js
+++ playground/static/js/compiler/util/character.js
@@ -1,6 +1,11 @@
 export class Character {
 
+    static toChar(c) {
+        return c[c.length - 1];
+    }
+
     static isNotLineTerminator(c) {
+        c = toChar(c);
         const carriageReturn = c === '\r';
         const lineFeed       = c === '\n';
 
@@ -8,10 +13,12 @@ export class Character {
     }
 
     static isDoubleQuotes(c) {
+        c = toChar(c);
         return c === '"';
     }
 
     static isWhitespace(c) {
+        c = toChar(c);
         // Everything below ' ' (x20) is considered a ASCII Control Character
         // which is not displayed, so I consider it also a whitespace.
         // And ' ' is also a whitespace, so we gotta use the <= operator.
@@ -19,6 +26,7 @@ export class Character {
     }
 
     static isLetter(c) {
+        c = toChar(c);
 
         // Lowercase
         if (c >= 'a' && c <= 'z')
@@ -32,6 +40,7 @@ export class Character {
     }
 
     static isDigit(c) {
+        c = toChar(c);
         return c >= '0' && c <= '9'
     }
 
