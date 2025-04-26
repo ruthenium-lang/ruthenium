6ef428099bb76ba4ad07a543138acf18957ea78b
CiroZDP <cirod307@gmail.com>
2025-04-26 16:08:42 +0200

    ➕ `character.js`: Add `isNotLineTerminator()`

--- playground/static/js/compiler/util/character.js
+++ playground/static/js/compiler/util/character.js
@@ -1,6 +1,7 @@
 export const Character = {
-    isDoubleQuotes:  c => (c === '"'),
-    isWhitespace:    c => (c <= ' '),
-    isLetter:        c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'),
-    isDigit:         c => (c >= '0' && c <= '9'),
+    isNotLineTerminator: c => (c === '\n') || (c === '\r'),
+    isDoubleQuotes:      c => (c === '"'),
+    isWhitespace:        c => (c <= ' '),
+    isLetter:            c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'),
+    isDigit:             c => (c >= '0' && c <= '9'),
 };
