1f7e063cd1aad612042bf28a708b1a7e433bad25
CiroZDP <cirod307@gmail.com>
2025-04-26 21:34:59 +0200

    🛠️ Convert `Character` to class

--- playground/static/js/compiler/util/character.js
+++ playground/static/js/compiler/util/character.js
@@ -1,7 +1,38 @@
-export const Character = {
-    isNotLineTerminator: c => (c !== '\n') && (c !== '\r'),
-    isDoubleQuotes:      c => (c === '"'),
-    isWhitespace:        c => (c <= ' '),
-    isLetter:            c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'),
-    isDigit:             c => (c >= '0' && c <= '9'),
+export class Character {
+
+    static isNotLineTerminator(c) {
+        const carriageReturn = c === '\r';
+        const lineFeed       = c === '\n';
+
+        return !(lineFeed || carriageReturn);
+    }
+
+    static isDoubleQuotes(c) {
+        return c === '"';
+    }
+
+    static isWhitespace(c) {
+        // Everything below ' ' (x20) is considered a ASCII Control Character
+        // which is not displayed, so I consider it also a whitespace.
+        // And ' ' is also a whitespace, so we gotta use the <= operator.
+        return c <= ' ';
+    }
+
+    static isLetter(c) {
+
+        // Lowercase
+        if (c >= 'a' && c <= 'z')
+            return true;
+
+        // Uppercase
+        if (c >= 'A' && c <= 'Z')
+            return true;
+
+        return false;
+    }
+
+    static isDigit(c) {
+        return c >= '0' && c <= '9'
+    }
+
 };
