ffda99ecca065a4d6d47248a2f9ae242a948310c
CiroZDP <cirod307@gmail.com>
2025-04-26 21:40:21 +0200

    ꜰᴇᴀᴛᴜʀᴇ: Convert Character to a class (#37)
    * 🛠️ Convert `Character` to class
    
    * ➕ `character.js`: Add function `toChar`

--- playground/static/js/compiler/util/character.js
+++ playground/static/js/compiler/util/character.js
@@ -1,7 +1,47 @@
-export const Character = {
-    isNotLineTerminator: c => (c !== '\n') && (c !== '\r'),
-    isDoubleQuotes:      c => (c === '"'),
-    isWhitespace:        c => (c <= ' '),
-    isLetter:            c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'),
-    isDigit:             c => (c >= '0' && c <= '9'),
+export class Character {
+
+    static toChar(c) {
+        return c[c.length - 1];
+    }
+
+    static isNotLineTerminator(c) {
+        c = toChar(c);
+        const carriageReturn = c === '\r';
+        const lineFeed       = c === '\n';
+
+        return !(lineFeed || carriageReturn);
+    }
+
+    static isDoubleQuotes(c) {
+        c = toChar(c);
+        return c === '"';
+    }
+
+    static isWhitespace(c) {
+        c = toChar(c);
+        // Everything below ' ' (x20) is considered a ASCII Control Character
+        // which is not displayed, so I consider it also a whitespace.
+        // And ' ' is also a whitespace, so we gotta use the <= operator.
+        return c <= ' ';
+    }
+
+    static isLetter(c) {
+        c = toChar(c);
+
+        // Lowercase
+        if (c >= 'a' && c <= 'z')
+            return true;
+
+        // Uppercase
+        if (c >= 'A' && c <= 'Z')
+            return true;
+
+        return false;
+    }
+
+    static isDigit(c) {
+        c = toChar(c);
+        return c >= '0' && c <= '9'
+    }
+
 };
