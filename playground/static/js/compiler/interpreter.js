437c4992c7fe7f8f240f70c2ac353147ac4f160d
CiroZDP <cirod307@gmail.com>
2025-04-27 22:12:27 +0200

    🔧 `interpreter.js`: Unwrap if conditions

--- playground/static/js/compiler/interpreter.js
+++ playground/static/js/compiler/interpreter.js
@@ -31,40 +31,40 @@ class Interpreter {
         switch (statement.type) {
 
             case 'VariableDeclaration':
-                if (statement.value) {
-                    let contains = "<RT_UNDEFINED>" /*, valueType*/;
-                    if (statement.value.isSurroundedBy('"')) {
-                        //valueType = 'STR_LITERAL';
-                        contains = statement.value.unwrap();
+                if (!statement.value)
+                    break;
 
-                    } else {
-                        //valueType = 'NUM_LITERAL';
-                        contains = parseInt(statement.value);
-                    }
-                    console.log(contains);
-                    this.env.id[statement.name] = contains;
-
+                let contains = "<RT_UNDEFINED>" /*, valueType*/;
+                if (statement.value.isSurroundedBy('"')) {
+                    //valueType = 'STR_LITERAL';
+                    contains = statement.value.unwrap();
+                } else {
+                    //valueType = 'NUM_LITERAL';
+                    contains = parseInt(statement.value);
                 }
+
+                console.log(contains);
+                this.env.id[statement.name] = contains;
                 break;
 
             case 'FunctionDeclaration':
-                if (statement.name === 'main') {
-                    for (const bodyNode of statement.body) {
-                        this.evaluate(bodyNode);
-                    }
+                if (statement.name !== 'main')
+                    break;
+
+                for (const bodyNode of statement.body) {
+                    this.evaluate(bodyNode);
                 }
                 break;
 
             case 'FunctionCall':
-                if (statement.name === 'println') {
-                    const output = document.getElementById('output');
-                    const value = statement.args[0].isSurroundedBy('"') ?
-                        statement.args[0].unwrap() :
-                        this.env.id[statement.args[0]];
-                    output.innerHTML += value + '\n';
-                }
-                break;
+                if (statement.name !== 'println')
+                    break;
 
+                const output = document.getElementById('output');
+                const value = statement.args[0].isSurroundedBy('"') ?
+                    statement.args[0].unwrap() :
+                    this.env.id[statement.args[0]];
+                output.innerHTML += value + '\n';
         }
     }
 
