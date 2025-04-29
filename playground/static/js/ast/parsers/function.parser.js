c4e2db4a7b4fe4779b34022e6e212277afc894ed
CiroZDP <cirod307@gmail.com>
2025-04-29 16:43:59 +0200

    ➖ Delete `function.template.js`

--- playground/static/js/ast/parsers/function.parser.js
+++ playground/static/js/ast/parsers/function.parser.js
@@ -2,16 +2,49 @@ export class ASTFunctionParser {
 
     constructor(tree, stream) {
         this.stream = stream;
-        this.tree   = tree;
+        this.tree = tree;
+    }
+
+    parseStructure() {
+        let obj = {
+            type: "FunctionDeclaration",
+            params: [],
+            returnType: "void",
+            body: []
+        };
+
+        this.stream.expect('fn');
+
+        if (!this.stream.peekTypeEquals("ID"))
+            return this.stream.error(Errors.AST.Fn_MissingIdentifier);
+
+        obj.name = this.stream.pop();
+        if (!this.stream.expect('('))
+            return this.stream.error(Errors.AST.Fn_MalformedArgs);
+
+        while (!this.stream.next(")")) {
+            if (!this.stream.peekTypeEquals(["TYPE", "ID"]))
+                return this.stream.error(Errors.TYPECHECK.Fn_InvalidArgType);
+
+            const name = this.stream.pop();
+            if (!this.stream.peekTypeEquals("ID"))
+                return this.stream.error(Errors.AST.Fn_InvalidArgName);
+
+            const value = this.stream.pop();
+            if (!this.stream.expect(','))
+                return this.stream.error(Errors.AST.Fn_MissingArgSeparator);
+
+            obj.params.push({ name: name, value: value });
+        }
+
+        if (this.stream.peekTypeEquals("ID"))
+            obj.returnType = this.stream.pop();
+
+        return obj;
     }
 
     parse() {
-        const fn = new ASTFunctionTemplate().fill(this.stream);
-        let block = {
-            type: "FunctionDeclaration",
-            body: [],
-            ...fn
-        };
+        let block = this.parseStructure();
 
         if (this.stream.peek() === '{') {
             block.body = this.parseBody();
@@ -41,8 +74,7 @@ export class ASTFunctionParser {
             return this.stream.error(Errors.AST.Fn_InvalidBody, 3), null;
 
         while (!this.stream.next("}")
-            && this.stream.remaining() > 0)
-        {
+            && this.stream.remaining() > 0) {
             const parser = new ASTParser(this.stream);
             body.push(...parser.parse());
         }
