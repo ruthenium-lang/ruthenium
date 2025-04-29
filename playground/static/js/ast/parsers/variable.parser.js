7528568ee8a555aad2e4802d4267d08712cd600a
CiroZDP <cirod307@gmail.com>
2025-04-29 18:22:54 +0200

    ➖ Delete `variable.template.js`

--- playground/static/js/ast/parsers/variable.parser.js
+++ playground/static/js/ast/parsers/variable.parser.js
@@ -6,8 +6,39 @@ export class ASTVariableParser {
     }
 
     parse() {
-        const variable = new ASTVariableTemplate().fill(this.stream);
+        const variable = this.parseStructure();
         this.tree.push({ type: "VariableDeclaration", ...variable });
+    }
+
+    parseStructure() {
+        let variable = {};
+
+        if (!this.stream.expect('let'))
+            return this.stream.error(Errors.AST.Let_MalformedDeclaration);
+
+        if (!this.stream.peekTypeEquals('ID'))
+            return this.stream.error(Errors.AST.Let_MissingIdentifier);
+
+        variable.name = this.stream.pop();
+        if (this.stream.next(';'))
+            return variable;
+
+        // Type declaration
+        if (this.stream.next(':')) {
+            if (!this.stream.peekTypeEquals("TYPE"))
+                return this.stream.error(Errors.TYPECHECK.Let_InvalidType);
+
+            variable.varType = this.stream.pop();
+        }
+
+        // Initialization
+        if (this.stream.next("="))
+            variable.value = this.stream.pop();
+
+        if (!this.stream.expect(';'))
+            return this.stream.error(Errors.AST.Statement_MissingEnd);
+
+        return variable;
     }
 
 }
