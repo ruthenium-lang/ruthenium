807fabbf93949d79de87e1c067873588b94b663c
CiroZDP <cirod307@gmail.com>
2025-04-30 18:44:34 +0200

    ⛔ ꜰɪx — Prevent null-type-inferring on specified types

--- playground/static/js/ast/constructors.js
+++ playground/static/js/ast/constructors.js
@@ -2,11 +2,9 @@ export function RTValue(content, type) {
     this.type = 'Value';
     this.content = content;
     if (content === undefined)
-    {
         this.type = 'EmptyValue';
-        this.valType = '?'; // Optional type
-    }
-    else this.valType = type ?? LitPattern.inferType(content);
+
+    this.valType = type ?? LitPattern.inferType(content);
 }
 
 export function RTExpression(content) {
