e583a1a77392123f603826bd5e42a0a9bad4898e
CiroZDP <cirod307@gmail.com>
2025-05-02 18:30:39 +0200

    ✨ ꜰᴇᴀᴛᴜʀᴇ — Variable initialization implemented!
    Yay!

--- playground/static/js/ast/constructors/assignment.constructor.js
+++ playground/static/js/ast/constructors/assignment.constructor.js
@@ -2,10 +2,15 @@ export function RTAssignment(targetVar, value) {
     if (targetVar.constant)
         return /*panic(),*/ false;
 
+    if (targetVar.varType !== value.valType) // TODO: qrtHasImplicitCast
+        return /*panic(),*/ false;
+
     return {
+        type: 'AssignmentOperation',
         eval: () => { targetVar.value = value; },
         initialization: () => {
             let obj = {
+                type: 'AssignmentOperation',
                 targetVar: targetVar.declaration(),
                 value
             }
