19e365607495f3efa3a42f952831f69ba8f04b39
CiroZDP <cirod307@gmail.com>
2025-05-09 22:42:12 +0200

    ➕ ᴛʏᴘᴇ.ᴘᴀᴛᴛᴇʀɴ.ᴊs — Add type template

---
+++ playground/static/js/ast/patterns/type.pattern.js
@@ -0,0 +1,10 @@
+class TypePattern {
+
+    static check(str) {
+        const types = ['int', 'uint', 'float'];
+        return types.indexOf(str) !== -1;
+    }
+
+}
+
+window.TypePattern = TypePattern;
