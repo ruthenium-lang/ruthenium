ae8bd137f68d956e330d90dd893c9ab63fdf52fe
CiroZDP <cirod307@gmail.com>
2025-05-10 21:15:08 +0200

    refactor: delete LitPattern

--- playground/static/js/ast/constructors/value.constructor.js
+++ playground/static/js/ast/constructors/value.constructor.js
@@ -1,10 +1,28 @@
+import { NameChecker } from "../checkers/name.checker.js";
+import { StrPattern } from "../checkers/string.checker.js";
+import { NumberType } from "../checkers/number.checker.js";
+
 export function RTValue(content, type) {
     this.type = 'Value';
     this.content = content;
     if (content === undefined)
         this.type = 'EmptyValue';
 
-    this.valType = type ?? LitPattern.inferType(content);
+    this.valType = type ?? inferType(content);
+}
+
+function inferType(str) {
+    if (StrPattern.test(str))
+        return 'string';
+
+    if (isNumber(str))
+        return NumberType.detect(str);
+
+    if (NameChecker.accepts(str))
+        return 'ID';
+
+    // TODO: error handling
+    return console.trace(), undefined;
 }
 
 window.RTValue = RTValue;
