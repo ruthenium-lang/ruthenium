fa7dc13678898be27a91d9c9b042c64d1ed01787
CiroZDP <cirod307@gmail.com>
2025-05-12 22:18:20 +0200

    chore: delete unused function `isIdentifier`

--- playground/static/js/compiler/util/tweaks.js
+++ playground/static/js/compiler/util/tweaks.js
@@ -1,9 +1,4 @@
-String.isIdentifier = input => {
-    const identifierRegex = /^[A-Za-z_][A-Za-z0-9_]*$/;
-    return identifierRegex.test(input);
-}
-
-String.prototype.unwrap = function () {
+String.prototype.unwrap = function() {
     return this.slice(1, -1);
 }
 
